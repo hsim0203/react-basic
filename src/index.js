07c70c67f0ce3f5d1c69ca7f38fa2ef668047894
hsim0203 <hsim0203@naver.com>
2024-05-21 17:54:52 +0900

    0521내용 추가

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';//App.js .js가 생략
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClassKaKaoMap from './components/ClassKaKaoMap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/classkakaomap" element={<ClassKaKaoMap />} />
+    </Routes>
+  </BrowserRouter>
 );
+  {/*   <React.StrictMode>
+      <App />
+</React.StrictMode> */}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
